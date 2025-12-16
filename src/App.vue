c056053df5429aad3b5172b6ce3e54a8da42a1dc
Damian Grela <deg171169@gmail.com>
2025-12-16 12:04:59 -0300

    modifique el App.vue y ademas realice el componente de answers, con las respuestas del cuestionario

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <Converter />
+    <Answers   />
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import Converter from './components/Converter.vue';
+import Answers from './components/Answers.vue';
+
+export default {
+  components: {
+    Converter,
+    Answers,
+  },
+};
+</script>
+
+<style>
+/* Estilos globales */
+</style>
---
+++ src/components/Answers.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Cuestionario</h1>
+    <p>Respuestas: 1:c 2:b 3:c 4:a 5:b</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Answers'
+};
+</script>
+
+<style scoped>
+/* Estilos opcionales */
+</style>
